5ede457911465ae59f03a1cabb54eb19fba56c88
ytokarevskaya <lan2214@yandex.ru>
2020-09-17 22:28:58 +0300

    Проверено

--- 10.4.1.js
+++ 10.4.1.js
@@ -4,4 +4,6 @@ for (i = str.length-1; i >= 0; i--) {
   strRev += str[i];
 }
 
+// Задание выполнено верно, но есть более простой способ:
 
+const reverseString = str.split("").reverse().join("");
--- 10.5.2.js
+++ 10.5.2.js
@@ -1,3 +1,19 @@
 let arr = [4, 8, 15, 16, 23, 42]
 const even = (element) => element % 2 === 0;
 console.log(arr.some(even));
+
+// Задача выполнена неверно, скорее всего, вы неправильно поняли условие. По заданию нужно было написать код, который определяет, одинаковые элементы в массиве или нет. Ваш код решает совсем другую задачу: определяет, все ли элементы в массиве четные.
+// Верное решение ниже:
+
+const arr = [1, 2, 3, 3];
+
+let isEqual = true;
+const reference = arr[0];
+
+for (let item of arr) {
+	if (item !== reference) {
+		isEqual = false;
+	}
+}
+
+console.log(isEqual);
